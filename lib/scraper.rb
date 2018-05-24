3cb9871db1adffd7c354713d7702b716c0b40da3
David Moore <hey@iamdavidmoore.com>
2018-05-24 18:33:19 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,7 @@ class Scraper
 
   def self.get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end
 
     def print_courses
@@ -22,3 +23,5 @@ class Scraper
   end
 
 end
+
+Scraper.new.get_page
